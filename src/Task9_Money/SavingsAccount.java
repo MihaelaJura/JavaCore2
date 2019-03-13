0902f3202be3ac35829000469586a04895ec184b
mjura <Mihaela.Jura@endava.com>
2019-03-13 16:58:07 +0200

    Added in SavingAccount the case where the amount in account is negative

--- src/Task9_Money/SavingsAccount.java
+++ src/Task9_Money/SavingsAccount.java
@@ -18,9 +18,15 @@ public class SavingsAccount {
     public void calculateMonthlyInterest() {
         System.out.println("Current savings balance: " + savingsBalance);
         double monthlyInterest;
-        monthlyInterest = (savingsBalance * annualInterestRate) / 12;
-        savingsBalance = monthlyInterest;
-        System.out.println("New savings balance: " + savingsBalance);
+        if(savingsBalance<=0){
+            annualInterestRate=annualInterestRate/2;
+            monthlyInterest = (savingsBalance * annualInterestRate) / 12;
+            savingsBalance = savingsBalance- monthlyInterest;
+            System.out.println("New savings balance: " + savingsBalance);}
+       else{
+           monthlyInterest = (savingsBalance * annualInterestRate) / 12;
+           savingsBalance = savingsBalance+ monthlyInterest;
+           System.out.println("New savings balance: " + savingsBalance);}
     }
 
     public double getBalance() {
@@ -36,6 +42,7 @@ class Driver {
     public static void main(String[] args) {
         SavingsAccount saver1 = new SavingsAccount(2000);
         SavingsAccount saver2 = new SavingsAccount(3000);
+        SavingsAccount saver3 = new SavingsAccount(-2000);
         saver1.modifyInterestRate(.04);
         saver1.calculateMonthlyInterest();
         saver2.modifyInterestRate(.04);
@@ -44,5 +51,9 @@ class Driver {
         saver1.calculateMonthlyInterest();
         saver2.modifyInterestRate(.05);
         saver2.calculateMonthlyInterest();
+
+        saver3.calculateMonthlyInterest();
+
+
     }
 }
